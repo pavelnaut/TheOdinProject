ee6b98bd572fd0cab13932d323c2a4a649999f72
pavelnaut <pavelmbogdanov@gmail.com>
2023-03-29 00:18:50 +0300

    Add color on hover

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,6 +1,30 @@
-const container = document.querySelector("#container");
-for (let i = 1; i <= 16*16; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
+function randomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
 }
+
+function createGrid() {
+    let container = document.querySelector("#container");
+    for (let i = 1; i <= 16*16; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      container.appendChild(square);
+    }
+}
+
+function addEventListeners() {
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+        let currentColor = square.style.backgroundColor || "rgb(255, 255, 255)";
+        if (currentColor === "rgb(255, 255, 255)") {
+            square.style.backgroundColor = randomColor();
+        }
+        });
+    });
+}
+
+createGrid();
+addEventListeners();
